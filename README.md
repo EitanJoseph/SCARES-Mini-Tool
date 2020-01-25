ca576dd493f3fec02e5b944d488910bb849ddd10
Chami <chami.lamelas@gmail.com>
2020-01-25 14:35:34 -0500

    added state geojson, started implementing

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ The CS Department's Class Tree in D3
 Links: 
 
 https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/42MVDX
+
+https://github.com/jgoodall/us-maps
---
+++ map.js
@@ -0,0 +1,35 @@
+// DEFINE VARIABLES
+// Define size of map group
+// Full world map is 2:1 ratio
+// Using 12:5 because we will crop top and bottom of map
+w = 3000;
+h = 1250;
+// variables for catching min and max zoom factors
+var minZoom;
+var maxZoom;
+
+// we can probably change it 
+var projection = d3
+   .geoEquirectangular()
+   .center([0, 15]) // set centre to further North
+   .scale([w/(2*Math.PI)]) // scale to fit group width
+   .translate([w/2,h/2]) // ensure centred in group
+;
+
+// map path
+var path = d3.geoPath().projection(projection);
+
+// zoom behavior
+
+function zoomed() {
+    t = d3.event.transform;
+    countriesGroup.attr("transform", "translate(" + [t.x,t.y] + ")scale(" + t.k + ")");
+}
+
+var zoom = d3.zoom().on("zoom", zoomed);
+
+function getTextBox(selection) {
+    selection.each(function(d) {
+        d.bbox = this.getBBox();
+    });
+}

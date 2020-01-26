31f70ead94ee5dfde0e15130805aec6c4f775c29
EitanJoseph <slamelas@bailey.cs.brandeis.edu>
2020-01-26 15:58:08 -0500

    added border to map,centered using new geoJson data

--- map.js
+++ map.js
@@ -3,8 +3,8 @@
       // Define size of map group
       // Full world map is 2:1 ratio
       // Using 12:5 because we will crop top and bottom of map
-      w = 3000;
-      h = 1250;
+      w = 1500;
+      h = 750;
       // variables for catching min and max zoom factors
       var minZoom;
       var maxZoom;
@@ -12,10 +12,11 @@
       // DEFINE FUNCTIONS/OBJECTS
       // Define map projection
       var projection = d3
-        .geoEquirectangular()
-        .center([0, 15]) // set centre to further North as we are cropping more off bottom of map
-        .scale([w / (2 * Math.PI)]) // scale to fit group width
-        .translate([w / 2, h / 2]) // ensure centred in group
+        .geoAlbersUsa()
+        // .geoEquirectangular()
+        // .center([0, 15])
+        // .scale([w/(2*Math.PI)])
+        .translate([w/2,h/2])
       ;
 
       // Define map path
@@ -137,7 +138,8 @@
 
       // get map data
       d3.json(
-        "state.geo.json",
+        "https://raw.githubusercontent.com/shawnbot/topogram/master/data/us-states.geojson",
+        //"state.geo.json",
         function(json) {
           //Bind data and create one path per GeoJSON feature
           countriesGroup = svg.append("g").attr("id", "map");
@@ -155,19 +157,21 @@
             .data(json.features)
             .enter()
             .append("path")
+          
             .attr("d", path)
             .attr("id", function(d, i) {
-              return "country" + d.properties.STUSPS10;
+              return "country" + d.properties.postal;
             })
             .attr("class", "country")
       //      .attr("stroke-width", 10)
       //      .attr("stroke", "#ff0000")
             // add a mouseover action to show name label for feature/country
             .on("mouseover", function(d, i) {
-                d3.select("#countryLabel" + d.properties.STUSPS10).style("visibility", "visible");
+                d3.select("#countryLabel" + d.properties.postal).style("visibility", "visible");
+                //console.log(d.properties.name)
             })
             .on("mouseout", function(d, i) {
-                d3.select("#countryLabel" + d.properties.STUSPS10).style("visibility", "hidden");
+                d3.select("#countryLabel" + d.properties.postal).style("visibility", "hidden");
             })
             // add an onclick action to zoom into clicked country
 
@@ -186,7 +190,7 @@
             .append("g")
             .attr("class", "countryLabel")
             .attr("id", function(d) {
-              return "countryLabel" + d.properties.STUSPS10;
+              return "countryLabel" + d.properties.postal;
             })
             .attr("transform", function(d) {
               return (
@@ -205,7 +209,7 @@
             
             .on("click", function(d, i) {
                 d3.selectAll(".country").classed("country-on", false);
-                d3.select("#country" + d.properties.STUSPS10).classed("country-on", true);
+                d3.select("#country" + d.properties.postal).classed("country-on", true);
             //  boxZoom(path.bounds(d), path.centroid(d), 20);
             });
           // add the text to the label group showing country name
@@ -216,13 +220,15 @@
             .attr("dx", 0)
             .attr("dy", 0)
             .text(function(d) {
-              return d.properties.NAME10;
+              return d.properties.name;
             })
             .call(getTextBox);
           // add a background rectangle the same size as the text
           countryLabels
             .insert("rect", "text")
             .attr("class", "countryLabelBg")
+            .attr("rx", 10)
+            .attr("ry", 10)
             .attr("transform", function(d) {
               return "translate(" + (d.bbox.x - 2) + "," + d.bbox.y + ")";
             })

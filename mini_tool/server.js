b4550ace2882fcd73beefa21316d192131daeea7
Chami <chami@turing.cs-i.brandeis.edu>
2020-04-24 17:51:18 -0400

    skeleton of mini tool

---
+++ mini_tool/server.js
@@ -0,0 +1,43 @@
+// This is the main file that sends data to the client and accesses the postgres database on turing
+
+var express = require("express");
+const { Client } = require("pg");
+
+// The client will be used to query the hejp postgres DB on turing using an environmental variable USERFLAG we can
+// run the program with the credentials of chami, ruth, or eitan
+client = new Client({
+  host: "/var/run/postgresql",
+  user: process.env.USERFLAG,
+  password: "",
+  database: "hejp"
+});
+
+// attempt to connect to server
+client.connect(err => {
+  if (err) {
+    console.error("connection error", err.stack);
+  } else {
+    console.log("connected");
+  }
+});
+
+// Initialize express app
+var app = express();
+
+// This will be used to parse the request body sent by the client to the server into a JSON array
+// N.B. the body parser is a separate NPM package that was installed
+var bodyParser = require("body-parser");
+
+// support json encoded bodies
+app.use(bodyParser.json());
+
+// Allows us to use scripts, views and organized for each visualization
+// views holds the ejs files for each visualization
+// scripts associated with the ejs files files for each visualization
+app.use(express.static(__dirname + "/"));
+
+// Marks that we will be using ejs templating
+app.set("view engine", "ejs");
+
+// server running on port 8000
+app.listen(7000);
--- slide-replication/test.js
+++
@@ -1,5 +0,0 @@
-module.exports = { 
-	logItUpBB: function(json) {
-		//document.getElementByID("test") = JSON.stringify(json)
-	}
-};	

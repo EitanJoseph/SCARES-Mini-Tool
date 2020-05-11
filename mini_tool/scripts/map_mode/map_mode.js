cdb29ceb4a9e85faab200fd536bb39a4cd434f55
Ruth <ruthros@brandeis.edu>
2020-05-11 17:03:58 -0400

    fixed colors

--- mini_tool/scripts/map_mode/map_mode.js
+++ mini_tool/scripts/map_mode/map_mode.js
@@ -11,9 +11,9 @@ var oldServerData
 var VISIBILITY_FACTOR = 1.6
 // the custom coloration for a state that was clicked on
 var clickedRGB = "rgb(186, 25, 0)"
-var Rscale = 253
-var Gscale = 164
-var Bscale = 43
+var Rscale = 71
+var Gscale = 186
+var Bscale = 83
 
 // last state that was clicked on
 var lastState = null
